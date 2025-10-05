79f7733d334f96f0eab2ad7af772448c6ea65b17
KarolisArliukas <karolis.arliukas@gmail.com>
2025-10-05 14:16:28 +0300

    Update index.jsx

--- 01. Static pages/04. First React Challenge/index.jsx
+++ 01. Static pages/04. First React Challenge/index.jsx
@@ -1,5 +1,1 @@
-/**
- * Challenge: Set up a React app from scratch again.
- * This time, try rendering an unordered list with 2-3 list items inside
- * with why you're excited to be learning React.
- */
+import {
